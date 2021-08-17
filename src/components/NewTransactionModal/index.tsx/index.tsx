ce5dbeb40157667556ef5cde0f6cc3d6a025530c
HemersonChaves <hemersonchaves20@hotmail.com>
2021-08-17 18:35:46 -0300

    feature Formulario e modal

--- src/components/NewTransactionModal/index.tsx/index.tsx
+++ src/components/NewTransactionModal/index.tsx/index.tsx
@@ -1,16 +1,34 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
+import closeImg from '../../../assets/fechar.svg'
 
-interface NewTransactionModalProps{
-    isOpen:boolean;
-    onRequestClose:()=>void;
+interface NewTransactionModalProps {
+    isOpen: boolean;
+    onRequestClose: () => void;
+    
 }
 
-export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModalProps){
-    return(
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    return (
         <Modal
-        isOpen={isOpen}
-        onRequestClose={onRequestClose}>
-        <h2>Cadastrar Informações</h2>
-      </Modal>
+            isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            overlayClassName="react-modal0overlay"
+            className ="react-modal-content"
+            >
+            <h2>Cadastrar Informações</h2>
+            <button type="button" onClick={onRequestClose} className="react-modal-close">
+                <img src={closeImg} alt="fechar modal" />
+            </button>
+            <Container>
+
+                <input placeholder="Título" ></input>
+                <input type="number" placeholder="Valor" ></input>
+                <input placeholder="Categoria" ></input>
+                <button type="submit"> cadastrar</button>
+            </Container>
+
+
+        </Modal>
     )
 }
