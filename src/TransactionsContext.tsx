3e87f2c16ddce799f7cb72d77748808eef23392c
HemersonChaves <hemersonchaves20@hotmail.com>
2021-08-31 12:10:59 -0300

    Movendo criação para o context

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -1,6 +1,5 @@
 import { createContext, useState,useEffect, ReactNode } from "react";
 import { api } from "./services/api";
-
 interface ITransaction{
     id:number;
     title:string;
@@ -9,11 +8,19 @@ interface ITransaction{
     category:string;
     createdAt:string;
 }
+type ITransactionInput = Omit<ITransaction, 'id'|'createdAt'>;
+//type ITransactionInput = Pick<ITransaction, 'title'|'amount'| 'category' |'type'>;
+
+interface ITransactionContextData{
+    transactions:ITransaction[];
+    createTransaction:(transaction:ITransactionInput)=>void;
+
+}
 interface TransactionProviderProps{
     children:ReactNode;
 }
 
-export const TransactionsContext = createContext<ITransaction[]>([]);
+export const TransactionsContext = createContext<ITransactionContextData>({}as ITransactionContextData);
 
 
 export function TransactionsProvider({children}:TransactionProviderProps){
@@ -24,8 +31,14 @@ export function TransactionsProvider({children}:TransactionProviderProps){
         .then(response => setTransactions(response.data.transactions))
     }, []);
 
+    function createTransaction(transactions: ITransactionInput){
+       
+        api.post('/transactions', transactions);
+
+    }
+
     return (
-        <TransactionsContext.Provider value={transactions}>
+        <TransactionsContext.Provider value={{transactions, createTransaction}}>
             {children}
         </TransactionsContext.Provider>
     );
